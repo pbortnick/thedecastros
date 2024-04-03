107f931d6f54c0f98ba7e6f8e6f4557366c6d3d4
pbortnick <pbortnick@gmail.com>
2024-04-03 19:08:57 -0400

    create gallery

---
+++ src/app/gallery/images/index.ts
@@ -0,0 +1,21 @@
+import gallery1 from './gallery-1.jpeg'
+import gallery2 from './gallery-2.jpeg'
+import gallery3 from './gallery-3.jpeg'
+import gallery4 from './gallery-4.jpeg'
+import gallery5 from './gallery-5.jpeg'
+import gallery6 from './gallery-6.jpeg'
+import gallery7 from './gallery-7.jpeg'
+import gallery8 from './gallery-8.jpeg'
+import gallery9 from './gallery-9.jpeg'
+
+export {
+	gallery1,
+	gallery2,
+	gallery3,
+	gallery4,
+	gallery5,
+	gallery6,
+	gallery7,
+	gallery8,
+	gallery9,
+}
--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,7 +1,59 @@
-const Gallery = () => {
+'use client'
+import 'photoswipe/dist/photoswipe.css'
+import PhotoSwipeLightbox from 'photoswipe/lightbox'
+import * as GalleryImages from './images'
+import { useEffect } from 'react'
+import s from './gallery.module.css'
+import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry'
+
+import Image, { StaticImageData } from 'next/image'
+const GALLERY_ID = 'pj-gallery'
+
+const chunkedGallery = Object.values(GalleryImages).reduce(
+	(resultArray, item) => {
+		if (!resultArray[0] || resultArray[0].length < 4) {
+			if (!resultArray[0]) {
+				resultArray[0] = []
+			}
+			resultArray[0].push(item)
+		} else if (!resultArray[1] || resultArray[1].length < 5) {
+			if (!resultArray[1]) {
+				resultArray[1] = []
+			}
+			resultArray[1].push(item)
+		} else if (!resultArray[2] || resultArray[2].length < 4) {
+			if (!resultArray[2]) {
+				resultArray[2] = []
+			}
+			resultArray[2].push(item)
+		}
+
+		return resultArray
+	},
+	[] as StaticImageData[][],
+)
+
+const GalleryPage = () => {
 	return (
-		<div>
-			<h1>Gallery</h1>
-		</div>
+		<main className={s.galleryPage}>
+			<div className={s.content}>
+				<h1>Gallery</h1>
+				<div className={s.gallery}>
+					<div className={s.galleryGridContainer}>
+						{chunkedGallery.map((chunk, index) => (
+							<div className={s.galleryGridSection}>
+								{chunk.map((image, index) => (
+									<div className={s.galleryGridItem} key={index}>
+										<Image {...image} alt="" className={s.galleryImage} />
+									</div>
+								))}
+							</div>
+						))}
+					</div>
+				</div>
+			</div>
+		</main>
 	)
 }
+
+export default GalleryPage
