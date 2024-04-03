f3940cd11e23b2faa0a2c5a4a790fbcd43b9f1ae
pbortnick <pbortnick@gmail.com>
2024-04-02 21:16:41 -0400

    homepage styles

--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -1,10 +1,17 @@
 'use client'
 
-import Link from 'next/link'
-import * as NavigationMenu from '@radix-ui/react-navigation-menu'
-import s from './nav.module.css'
 import classNames from 'classnames'
+import Link from 'next/link'
 import { usePathname } from 'next/navigation'
+import * as NavigationMenu from '@radix-ui/react-navigation-menu'
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
+import {
+	HamburgerMenuIcon,
+	DotFilledIcon,
+	CheckIcon,
+	ChevronRightIcon,
+} from '@radix-ui/react-icons'
+import s from './nav.module.css'
 
 const PAGES = [
 	{ name: 'Home', href: '/' },
@@ -18,7 +25,7 @@ const MenubarDemo = () => {
 	const currentPath = usePathname()
 	return (
 		<div className={s.nav}>
-			<NavigationMenu.Root>
+			<NavigationMenu.Root className={s.desktopNav}>
 				<NavigationMenu.List className={s.navList}>
 					{PAGES.map((page) => (
 						<Link
@@ -34,6 +41,25 @@ const MenubarDemo = () => {
 					))}
 				</NavigationMenu.List>
 			</NavigationMenu.Root>
+
+			<DropdownMenu.Root>
+				<DropdownMenu.Trigger asChild className={s.mobileNav}>
+					<button className="IconButton" aria-label="Toggle nav">
+						<HamburgerMenuIcon />
+					</button>
+				</DropdownMenu.Trigger>
+				<DropdownMenu.Portal>
+					<DropdownMenu.Content className={s.dropdownContent} sideOffset={5}>
+						{PAGES.map((page) => (
+							<DropdownMenu.Item key={page.name}>
+								<Link href={page.href} className={s.link}>
+									{page.name}
+								</Link>
+							</DropdownMenu.Item>
+						))}
+					</DropdownMenu.Content>
+				</DropdownMenu.Portal>
+			</DropdownMenu.Root>
 		</div>
 	)
 }
