4a8339b12f1a3f507a49fc7e88fb2eaaffb811ba
Pamela Bortnick <pbortnick@gmail.com>
2024-10-01 14:58:36 -0400

    rsvp widget

---
+++ src/app/(site)/rsvp/page.tsx
@@ -0,0 +1,13 @@
+// pages/index.js
+import RsvpWidget from './components/rsvp-widget'
+
+const Home = () => {
+	return (
+		<div>
+			<h1>Welcome to Our RSVP Page</h1>
+			<RsvpWidget eventId="IUZIpFfYkHlXZSYa11Et" />
+		</div>
+	)
+}
+
+export default Home
