7e3cdbef3032efdd74ee1f4d997d0f1c800abb7c
Pamela Bortnick <pbortnick@gmail.com>
2024-10-13 20:07:18 -0400

    add text

--- src/app/(site)/rsvp/page.tsx
+++ src/app/(site)/rsvp/page.tsx
@@ -5,6 +5,9 @@ const Home = () => {
 	return (
 		<>
 			<Hero title="RSVP" />
+			<div style={{ margin: '0 auto', maxWidth: '90%' }}>
+				<p>If the form below does not load, please refresh the page.</p>
+			</div>
 			<RsvpWidget />
 		</>
 	)
