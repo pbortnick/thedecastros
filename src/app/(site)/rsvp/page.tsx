0aae798e19b00910d2441c2f53d7c9d1777ab7d4
Pamela Bortnick <pbortnick@gmail.com>
2024-10-13 20:08:56 -0400

    fix

--- src/app/(site)/rsvp/page.tsx
+++ src/app/(site)/rsvp/page.tsx
@@ -5,7 +5,7 @@ const Home = () => {
 	return (
 		<>
 			<Hero title="RSVP" />
-			<div style={{ margin: '0 auto', maxWidth: '90%' }}>
+			<div style={{ margin: '0 auto', maxWidth: '90%', width: 'fit-content' }}>
 				<p>If the form below does not load, please refresh the page.</p>
 			</div>
 			<RsvpWidget />
