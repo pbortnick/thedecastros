a64e079345d47f4e2b806124b4d1ac465c6708c4
Pamela Bortnick <pbortnick@gmail.com>
2024-10-16 19:38:08 -0400

    add backup

---
+++ src/app/(site)/rsvp/components/rsvp-widget/backup.tsx
@@ -0,0 +1,21 @@
+// 'use client';
+
+// import Script from 'next/script';
+//     <>
+//       <Script
+//         type="text/javascript"
+//         id=""
+//         dangerouslySetInnerHTML={{
+//           __html: `
+//           (function (w, d, s, o, f, js, fjs) {
+//             w[o] = w[o] || function () { (w[o].q = w[o].q || []).push(arguments) };
+//             js = d.createElement(s), fjs = d.getElementsByTagName(s)[0];
+//             js.id = o; js.src = f; js.async = 1; fjs.parentNode.insertBefore(js, fjs);
+//         }(window, document, 'script', 'oursvp', 'https://www.oursvp.app/widget/widget.js'));
+//         oursvp('init', { 
+//             element: document.getElementById('oursvp-widget'),
+//             eventId: '',
+//             showBackBtn: false
+//         });`
+//         }}
+//       /> 
